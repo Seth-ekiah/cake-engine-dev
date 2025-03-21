c997a261ccf03e441ad261967d8ab86d7815a277
Caleb Whitmer <whityj375@gmail.com>
2025-03-21 13:27:09 -0400

    Added my current templates

--- .codedoc/theme.ts
+++ .codedoc/theme.ts
@@ -1,11 +1,47 @@
-import { createTheme } from '@codedoc/core/transport';
+// Batter (Light): ffe9ce
+// Batter (Dark): e5d0b7
+// Frosting (Light): ffa1bb
+// Frosting (Dark): ff6d96
+
+import {
+  createTheme,
+} from '@codedoc/core/transport';
 
 
 export const theme = /*#__PURE__*/createTheme({
   light: {
-    primary: '#1eb2a6'
+    primary: '#ff6d96',
+    background: '#ffe9ce',
+    code: '#ff6d96',
   },
   dark: {
-    primary: '#1eb2a6'
+    primary: '#ff6d96',
+  },
+  code: {
+    wmbar: false,
+    light: {
+      text: '#0f0f0f',
+      background: '#e5d0b7',
+      lineHover: '#ffa1bb',
+    },
+  },
+  quote: {
+    light: {
+      background: '#e5d0b7',
+    },
+  },
+  toc: {
+    light: {
+      background: '#ffa1bb',
+    },
   }
+
+
+
 });
+
+// export interface QuoteTheme {
+//   background: '#e5c49b';             // --> CSS color string, background of block-quotes
+//   // text: string;                   // --> CSS color string, text color of block-quotes
+//   // border: string;                 // --> CSS color string, border color for block-quotes
+// }
--- docs/md/index.md
+++ docs/md/index.md
@@ -1,1 +1,3 @@
-# Hello World!
+<!-- ![cake](docs/assets/images/logo/cake_Logo.png =250x250) -->
+<!-- <img src="docs/assets/images/logo/cake_Logo.png" alt="drawing" style="width:100px;"/> -->
+# Welcome to cake Engine
